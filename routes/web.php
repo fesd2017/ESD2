8f8ead492bdbe3ad2941f982249c5f873035b2ac
3A432053許漢昌 <s3a432053student.ncut.edu.tw>
2017-11-17 19:28:37 +0800

    刪除自建new方法，增加查詢資料方法

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,10 @@
 
 Route::get('/', function () {
     return view('welcome');
+
 });
 
-Route::get('/BackGround',function () {
-    $BackGround	=	new	\App\BackGround();
-    $BackGround->student_id	=	'1';
-    $BackGround->name	=	'哇哈哈';
-    $BackGround->account	=	'3115318';
-    $BackGround->password	=	'1513153';
-    $BackGround->phone	=	'0911225484';
-    $BackGround->email	=	'5542184@mgail.com';
-    $BackGround->sex	=	'0';
-    $BackGround->class	=	'四資三乙';
-    $BackGround->address	=	'台中市西屯區XX路';
-    $BackGround->save();
+Route::get('/Student',function () {
+    $Student = \App\Student::all();
+    dd($Student);
 });
