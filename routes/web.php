1a186e7aa6623920b60eadbef05b86240a21e690
3A432053許漢昌 <s3a432053student.ncut.edu.tw>
2017-11-17 18:27:25 +0800

    在route新增students資料表內的資料

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,15 @@ Route::get('/', function () {
 });
 
 Route::get('/BackGround',function () {
-    $post = new \App\BackGround();
-
-    $post = BackGround::all();
+    $BackGround	=	new	\App\BackGround();
+    $BackGround->student_id	=	'1';
+    $BackGround->name	=	'哇哈哈';
+    $BackGround->account	=	'3115318';
+    $BackGround->password	=	'1513153';
+    $BackGround->phone	=	'0911225484';
+    $BackGround->email	=	'5542184@mgail.com';
+    $BackGround->sex	=	'0';
+    $BackGround->class	=	'四資三乙';
+    $BackGround->address	=	'台中市西屯區XX路';
+    $BackGround->save();
 });
